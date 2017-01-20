ea22b521955a13c374ef14c285ccd1680208b648
Caleb Fangmeier <caleb.fangmeier@gmail.com>
2017-01-20 11:29:57 -0600

    switches to data-as-code for easier distribution

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python
-
 import tkinter as tk
 from random import choice
-import json
+
+from game_data import adjatives, people, situations
 
 
 class Application(tk.Frame):
     def __init__(self, master=None):
-        super().__init__(master)
+        tk.Frame.__init__(self, master)
         self.pack()
         self.create_widgets()
-        self.people = self.load_json("people.json")
-        self.adjatives = self.load_json("adjatives.json")
-        self.situations = self.load_json("situations.json")
-
-    def load_json(self, filename):
-        with open(filename) as file_:
-            return json.load(file_)
 
     def create_widgets(self):
         self.next = tk.Button(self)
@@ -34,14 +27,15 @@ class Application(tk.Frame):
         self.quit.pack(side="bottom")
 
     def update_text(self):
-        text = '  '.join([choice(self.adjatives),
-                         choice(self.people),
-                         choice(self.situations)])
+        text = '  '.join([choice(adjatives),
+                         choice(people),
+                         choice(situations)])
         self.label["text"] = text
 
 
-root = tk.Tk()
-app = Application(master=root)
-app.master.title("Second Impressions")
-app.master.minsize(800, 300)
-app.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Application(master=root)
+    app.master.title("Second Impressions")
+    app.master.minsize(800, 300)
+    app.mainloop()
